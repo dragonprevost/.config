9035cd3cf935c6ba37761e03f1cb1a0abd215c12
Dragon Prevost <dragon@dkp.io>
2025-07-11 09:52:01 -0700

    Cleaned up lsp configs

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,47 +1,43 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.gopls.setup({capabilities = capabilities})
-			lspconfig.ts_ls.setup({capabilities = capabilities})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-				python = {
-					analysis = {
-						autoSearchPaths = true,
-						useLibraryCodeForTypes = true,
-						autoImportCompletions = true,
-					},
-					pythonPath = vim.fn.getcwd() .. "/venv/bin/python3",
-				},
-			})
-			vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>m", vim.diagnostic.open_float)
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>gR", vim.lsp.util.rename, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set("n", "<leader>mv", vim.lsp.util.rename)
-				end,
-			})
-      vim.filetype.add({
-        extension = {
-          graphql = "graphql",
-          gql = "graphql",
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.gopls.setup({ capabilities = capabilities })
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        python = {
+          analysis = {
+            autoSearchPaths = true,
+            useLibraryCodeForTypes = true,
+            autoImportCompletions = true,
+          },
+          pythonPath = vim.fn.getcwd() .. "/venv/bin/python3",
         },
       })
-		end,
-	},
+
+      vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>m", vim.diagnostic.open_float)
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>gR", vim.lsp.util.rename, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>mv", vim.lsp.util.rename)
+        end,
+      })
+    end,
+  },
 }
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
+      debug = true,   -- turn on debugging
       autostart = true,
       sources = {
         -- null_ls.builtins.diagnostics.mypy,
@@ -10,7 +11,8 @@ return {
         -- null_ls.builtins.diagnostics.ruff,
         -- null_ls.builtins.formatting.black,
         -- null_ls.builtins.diagnostics.erb_lint,
-        -- null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.formatting.prettier,
+        -- null_ls.builtins.formatting.eslint_d,
         -- null_ls.builtins.diagnostics.rubocop,
         -- null_ls.builtins.formatting.rubocop,
         -- null_ls.builtins.formatting.stylua,
